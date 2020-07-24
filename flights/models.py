e0da6601a44f9233dcf5ecaed7fc0f6f9357dc77
Nacho <ignacioga.346@gmail.com>
2020-07-24 04:52:27 -0300

    Try fix error2

--- flights/models.py
+++ flights/models.py
@@ -9,9 +9,10 @@ class Airport(models.Model):
 		return f"{self.city} ({self.code})"
 
 class Flight(models.Model):
-	origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
-	destination= models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
-	duration = models.IntegerField()
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
 
 	def __str__(self):
 		return f"{self.id}: {self.origin} to {self.destination}"
